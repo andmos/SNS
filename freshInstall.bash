25d610f8c8de891275541e481b58dab7743f452a
Andreas Mosti <andreas.mosti@gmail.com>
2013-10-16 22:53:20 +0200

    Added Vagrantfile for project with webserver install

---
+++ freshInstall.bash
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+function install() {
+sudo apt-get install -y $1 >/dev/null 2>&1 
+    }
+
+programs="build-essential vim tor  ssh curl  htop git make traceroute tcptraceroute screen python-dev apache2 php5" 
+
+echo "Installing web-server essentials..."
+sudo apt-get update >/dev/null 2>&1 
+
+for program in $programs
+do 
+    echo "Current package: $program" 
+    install $program 
+
+done 
+
+echo "Symlincing /www folder to /Vagrant folder..."
+
+rm -rf /var/www
+ln -fs /vagrant /var/www 
+
+echo "All done! go 'vagrant ssh'" 
