0a03da9eab8d4645bfa4b1e553fb5a81a5994ac8
Sureface <thomas.mowatt@gmail.com>
2013-10-29 13:56:59 +0100

    Endra login.php og loginPage.html, skal fungere utan crossite.

--- login.php
+++ login.php
@@ -1,15 +1,18 @@
+<html>
+<body>
+<center>
+	<h1>Velkommen til din bank</h1><br><br>
 <?php
-
-    $username = "root";
-    $password = "Glittertind";
-    $hostname = "localhost";	
-    $database = "konto";
+    	$username = "root";
+    	$password = "Glittertind";
+    	$hostname = "localhost";
 
 	$con = mysql_connect($hostname, $username, $password) or die("Unable to connecto to MySQL");
-
-        
-    $selected = mysql_select_db("konto", $con) or die("Could not select konto");
-       $query = mysql_query("Select * FROM konto"); 
-        echo "kake";  
-
+   	$selected = mysql_select_db("konto", $con) or die("Could not select konto");
+   	$query = mysql_query("Select * FROM konto"); 
+        echo "kake";
 ?>
+
+</center>
+</body>
+</html>
