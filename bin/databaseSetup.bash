aa3f916c03d390049819b04c77a834be8c8bf7f5
Andreas Mosti <andreas.mosti@gmail.com>
2013-10-29 21:08:59 +0100

    Beginning snort - install

--- bin/databaseSetup.bash
+++ bin/databaseSetup.bash
@@ -2,7 +2,9 @@
 
 pass=$(</vagrant/mysqlpw.txt)
 
-echo "Importing database:"
+echo "Importing databases:"
+echo "konto: "
 mysql -u root --password=$pass  -e 'create database konto'
 mysql -u root --password=$pass  konto < /vagrant/mysqldb.konto.sql
+
 echo "Done, ready to be SQL-Injected!"
--- bin/snortSetup.bash
+++ bin/snortSetup.bash
@@ -1,9 +1,27 @@
 #! /bin/bash
 
+#------------THIS FILE NEEDS WORKING ON, NOT FINNISHED.
+
 pass=$(</vagrant/mysqlpw.txt)
 
-echo "Setting up database:"
+echo "Setting up  snort-database:"
+
 mysql -u root --password=$pass -e 'create database snort'
+
+function setParameter(){
+        sudo debconf-set-selection <<< $1
+}
+
+
+setParameter echo "snort-mysql snort-mysql/db_pass $password"
+setParameter echo "snort-mysql snort-mysql/address_range   string  127.0.0.1/32" 
+setParameter echo "snort-mysql snort-mysql/send_stats  boolean true"
+setParameter echo "snort-mysql snort-mysql/please_restart_manually note"
+setParameter echo "snort-mysql snort-mysql/configure_db    boolean false"
+
+sudo apt-get install snort-mysql -y
+
 cd /usr/share/doc/snort-mysql/
-zcat create_mysql.gz | mysql -u <user> -h localhost --password=$pass  snort
+
+zcat create_mysql.gz | mysql -u root -h localhost --password=$pass  snort
 echo "Standard Snort-mysql DB configured" 
