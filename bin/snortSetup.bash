9ff5cd388b8d624ace319d3c709772524f98446e
Andreas Mosti <andreas.mosti@gmail.com>
2013-10-30 13:33:56 +0100

    Added template

--- bin/snortSetup.bash
+++ bin/snortSetup.bash
@@ -11,11 +11,6 @@ echo "Setting up  snort-database:"
 
 mysql -u root --password=$pass -e 'create database snort'
 
-sudo debconf-set selections <<< "snort-mysql snort-mysql/address_range string $SNORTIP/32"
-sudo debconf-set selections <<< "snort-mysql snort-mysql/configure_db boolean false"
-sudo debconf-set-selections <<<  "snort-mysql snort-mysql/db_host string localhost"
-sudo debconf-set-selections <<<  "snort-mysql snort-mysql/db_user string root"
-
 sudo apt-get install snort-mysql -y
 
 cd /usr/share/doc/snort-mysql/
