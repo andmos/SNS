43525f0c358f7c201895c2b4f1c4617126e3d932
Sureface <thomas.mowatt@gmail.com>
2014-01-07 11:00:42 +0100

    Made the loginpage for sql-Injection

---
+++ bank.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<center>
+<h1> Velkommen til din bank  </h1>
+
+<?
+
+echo "Navn: <br>";
+echo "Her er din saldo: ";
+    
+?>
+
+</center>
+</body>
+</html>
+
--- login.php
+++ login.php
@@ -1,16 +1,54 @@
+<!DOCTYPE html>
 <html>
+<head>
+</head>
 <body>
-<center>
-	<h1>Velkommen til din bank</h1><br><br>
+<center> 
+        
 <?php
-    	$username = "root";
+        $username = "root";
     	$password = "Glittertind";
     	$hostname = "localhost";
+        $loginUsername = $_POST["loginUsername"];
 
 	$con = mysql_connect($hostname, $username, $password) or die("Unable to connecto to MySQL");
-   	$selected = mysql_select_db("konto", $con) or die("Could not select konto");
-   	$query = mysql_query("Select * FROM konto"); 
-        echo "kake";
+	$selected = mysql_select_db("konto", $con) or die("Could not select konto");
+   
+        /**
+	$result = mysql_query("SELECT * FROM konto", $con);
+	while($row = mysql_fetch_array($result)){
+		echo $row['kontoeier'] . " " . $row['passord'];
+		echo "<br>";
+	}
+        
+         * 
+         */
+        
+        //Denne fungerer no (simpel), utan feilmelding.
+        $result = mysql_query("SELECT kontoeier, passord FROM konto", $con);
+        while($row = mysql_fetch_array($result)){
+            if($row['kontoeier'] == $loginUsername){
+                if($row['passord'] == $_POST["loginPassword"]){
+                    header("Location: http://localhost:8080/bank.php");
+                }
+            }
+        }
+        
+        function hentUsername(){
+            return $loginUsername;
+        }
+        
+        function hentSaldo(){
+            $result2 = mysql_query("SELECT saldo FROM konto WHERE kontoeier = '" . $loginUsername . "'", $con);
+            while($row = mysql_fetch_array($result2)){
+                if($row['saldo'] != null){
+                    return $result2;
+                }
+            }
+        }
+        
+        
+        
 ?>
 
 </center>
