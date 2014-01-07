0d5413113d62bd764de0e83c1e7f7605f38ef58b
Sureface <thomas.mowatt@gmail.com>
2014-01-07 13:43:10 +0100

    Havnt done much.

--- login.php
+++ login.php
@@ -13,23 +13,13 @@
 
 	$con = mysql_connect($hostname, $username, $password) or die("Unable to connecto to MySQL");
 	$selected = mysql_select_db("konto", $con) or die("Could not select konto");
-   
-        /**
-	$result = mysql_query("SELECT * FROM konto", $con);
-	while($row = mysql_fetch_array($result)){
-		echo $row['kontoeier'] . " " . $row['passord'];
-		echo "<br>";
-	}
-        
-         * 
-         */
         
         //Denne fungerer no (simpel), utan feilmelding.
         $result = mysql_query("SELECT kontoeier, passord FROM konto", $con);
         while($row = mysql_fetch_array($result)){
             if($row['kontoeier'] == $loginUsername){
                 if($row['passord'] == $_POST["loginPassword"]){
-                    header("Location: http://localhost:8080/bank.php");
+                    header("Location: bank.php");
                 }
             }
         }
